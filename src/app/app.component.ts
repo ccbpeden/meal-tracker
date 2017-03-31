7910204ca6b9fe584b39ba57e747585dfa359b4e
Charles Peden <ccbpeden@warpmail.net>
2017-03-31 09:35:34 -0700

    implement ability to add a meal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,19 @@ import { Meal } from './meal.model';
 })
 export class AppComponent {
   title = 'A Meal Tracker!';
+  newMealForm = false;
   parentMeals: Meal[] = [
     new Meal('Breakfast', '2 bowls Quinoa & Dates', 476),
     new Meal('Elevenses', 'icecream, cheetos, and fudge', 1325),
     new Meal('Lunch', 'asparagus, toast, lox', 563)
   ]
+
+  showNewMealForm(){
+    this.newMealForm = true;
+    console.log(this.newMealForm);
+  }
+  addMeal(name, details, calories){
+    var newMeal = new Meal(name, details, calories);
+    this.parentMeals.push(newMeal);
+  }
 }
