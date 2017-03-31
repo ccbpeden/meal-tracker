9b09d2a72cd568b59433251898a63c89896c3715
Charles Peden <ccbpeden@warpmail.net>
2017-03-31 15:05:11 -0700

    migrate content from app.component to welcome.component to clean up routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,29 +8,4 @@ import { Meal } from './meal.model';
 })
 export class AppComponent {
   title = 'A Meal Tracker!';
-  parentSelectedMeal = null;
-  parentNewMealForm = false;
-  parentMeals: Meal[] = [
-    new Meal('Breakfast', '2 bowls Quinoa & Dates', 476),
-    new Meal('Elevenses', 'icecream, cheetos, and fudge', 1325),
-    new Meal('Lunch', 'asparagus, toast, lox', 563)
-  ]
-
-  addMeal(params){
-    var newMeal = new Meal(params.name, params.details, params.calories);
-    this.parentMeals.push(newMeal);
-    this.parentNewMealForm = false;
-  }
-
-  showNewMealForm(){
-    this.parentNewMealForm = true;
-  }
-
-  selectMeal(meal){
-    this.parentSelectedMeal = meal;
-  }
-
-  closeEditForm(){
-    this.parentSelectedMeal = null;
-  }
 }
--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Meal } from '../meal.model';
 
 @Component({
   selector: 'app-welcome',
@@ -6,5 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent {
+  parentSelectedMeal = null;
+  parentNewMealForm = false;
+  parentMeals: Meal[] = [
+    new Meal('Breakfast', '2 bowls Quinoa & Dates', 476),
+    new Meal('Elevenses', 'icecream, cheetos, and fudge', 1325),
+    new Meal('Lunch', 'asparagus, toast, lox', 563)
+  ]
 
+  addMeal(params){
+    var newMeal = new Meal(params.name, params.details, params.calories);
+    this.parentMeals.push(newMeal);
+    this.parentNewMealForm = false;
+  }
+
+  showNewMealForm(){
+    this.parentNewMealForm = true;
+  }
+
+  selectMeal(meal){
+    this.parentSelectedMeal = meal;
+  }
+
+  closeEditForm(){
+    this.parentSelectedMeal = null;
+  }
 }
